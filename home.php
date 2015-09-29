6051d5b7ef21fb0d82f9547141e6f094e0626d32
jimmyLi <simi0058@EALPC-003413.eal.local>
2015-09-29 13:22:58 +0200

    added bootstrap

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tutoring Portal</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet" />
+<meta charset="UTF-8">
+</head>
+<body>
+<div class="container">
+<header>
+<div class="slider"></div>
+<div class="menubar">
+<nav>
+	<ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Workshops</a></li>
+		<li><a href="#">Tutoring</a></li>
+	</ul>
+</nav>
+</div>
+</header>
+
+</div>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
