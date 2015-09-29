98b9ed87003b31821d6e28f37a4f58278b20fd29
jimmyLi <simi0058@EALPC-003413.eal.local>
2015-09-29 12:11:22 +0200

    added style

--- index.php
+++ index.php
@@ -2,11 +2,26 @@
 <html>
 <head>
 <title>Tutoring Portal</title>
-<link href="style.css" type="stylesheet" />
+<link href="style.css" rel="stylesheet" />
 <meta charset="UTF-8">
 </head>
 <body>
+<div class="container">
+<header>
+<div class="slider">
 
+</div>
+<div class="menubar">
+<nav>
+	<ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Workshops</a></li>
+		<li><a href="#">Tutoring</a></li>
+	</ul>
+</nav>
+</div>
+</header>
 
+</div>
 </body>
 </html>
