326819ee1416284a29b39452051a9801b2dda7bc
jimmyLi <liviu.c.simion@gmail.com>
2015-09-30 11:44:15 +0200

    updated navbar

--- home.php
+++ home.php
@@ -11,47 +11,43 @@
 <header>
 <div class="slider"></div>
 <nav class="navbar navbar-default" data-spy="affix" data-offset-top="290">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+		</button>
+	</div>
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">
-      <ul class="nav navbar-nav navbar">
-			<li class="dropdown">
-			  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Username<span class="caret"></span></a>
-			  <ul class="dropdown-menu">
-				<li><a href="#">User Profile</a></li>
-				<li><a href="#">Logout</a></li>
-			  </ul>
-			</li>
-		</ul>
-	  </a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <form class="navbar-form navbar-right" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Workshops</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tutoring<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Offers</a></li>
-            <li><a href="#">Requests</a></li>
-          </ul>
-        </li>
-      </ul>
+				<ul class="nav navbar-nav navbar">
+					<li class="dropdown">
+					  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Username<span class="caret"></span></a>
+					  <ul class="dropdown-menu">
+						<li><a href="#">User Profile</a></li>
+						<li><a href="#">Logout</a></li>
+					  </ul>
+					</li>
+				</ul>
+			<form class="navbar-form navbar-right" role="search">
+				<div class="form-group">
+				  <input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Workshops</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tutoring<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Offers</a></li>
+						<li><a href="#">Requests</a></li>
+					</ul>
+				</li>
+			</ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
@@ -64,11 +60,48 @@
 				<div class="panel-heading">Upcomming workshops</div>
 				<div class="panel-body">
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<p>Workshop #1</p>
-					<p>Workshop #2</p>
-					<p>Workshop #3</p>
-					<p>Workshop #4</p>
-					<p>Workshop #5</p>
+					<div class="row">
+						<div class="col-xs-4 col-md-5 col-lg-4">
+							<img src="img/placeholder.jpg" alt="Image" />
+						</div>
+						<div class="col-xs-8 col-md-7 col-lg-8">
+							<h3>PHP Introduction Workshop</h3>
+							<p>Date: Monday, 1st of Juyl 2016</p>
+							<p>Location: MMD Building 8, Room 25</p>
+							<p>Hours: 12:00 - 15:00</p>
+							<p>Tutor: John Doe</p>
+							<p>Decription: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+						</div>
+					</div>
+					</br>
+					<div class="row">
+						<div class="col-xs-4 col-md-5 col-lg-4">
+							<img src="img/placeholder.jpg" alt="Image" />
+						</div>
+						<div class="col-xs-8 col-md-7 col-lg-8">
+							<h3>PHP Introduction Workshop</h3>
+							<p>Date: Monday, 1st of Juyl 2016</p>
+							<p>Location: MMD Building 8, Room 25</p>
+							<p>Hours: 12:00 - 15:00</p>
+							<p>Tutor: John Doe</p>
+							<p>Decription: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+						</div>
+					</div>
+					</br>
+					<div class="row">
+						<div class="col-xs-4 col-md-5 col-lg-4">
+							<img src="img/placeholder.jpg" alt="Image" />
+						</div>
+						<div class="col-xs-8 col-md-7 col-lg-8">
+							<h3>PHP Introduction Workshop</h3>
+							<p>Date: Monday, 1st of Juyl 2016</p>
+							<p>Location: MMD Building 8, Room 25</p>
+							<p>Hours: 12:00 - 15:00</p>
+							<p>Tutor: John Doe</p>
+							<p>Decription: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+						</div>
+					</div>
+					</br>
 				</div>
 			</div>
   </div>
@@ -76,13 +109,13 @@
 		<div class="panel panel-default">
 			<div class="panel-heading">Latest offers</div>
 			<div class="panel-body">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				<p>Lorem ipsum dolor sit amet, ornare lacus congue, laoreet accumsan luctus sed in amet, litora eget ut libero, cursus dui sit ultrices nibh aliquam quam, felis rutrum dolor in at vestibulum. Morbi lorem non scelerisque. Viverra sollicitudin quam id lectus, non semper aperiam odio id adipiscing, proin libero, mollis sed nunc vulputate purus in. Nibh non nulla, pretium aenean in ipsum neque placerat congue, eleifend ornare quia ut in eos libero. Volutpat eros, commodo erat quam elit, sit laoreet nibh eum velit. Nibh pellentesque, purus amet congue. Pede felis sed felis, platea proin sodales sodales. Aliquam laoreet beatae congue, sed nulla bibendum sit lorem vitae, nec interdum posuere amet viverra eros. Consequat phasellus magna purus molestie justo orci, donec nibh lacus felis egestas quam, eget ut vitae, in suspendisse, duis eget vestibulum porta eget suspendisse sed.</p>
 			</div>
 		</div>
 		<div class="panel panel-default">
 			<div class="panel-heading">Latest requests</div>
 			<div class="panel-body">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				<p>Lorem ipsum dolor sit amet, ornare lacus congue, laoreet accumsan luctus sed in amet, litora eget ut libero, cursus dui sit ultrices nibh aliquam quam, felis rutrum dolor in at vestibulum. Morbi lorem non scelerisque. Viverra sollicitudin quam id lectus, non semper aperiam odio id adipiscing, proin libero, mollis sed nunc vulputate purus in. Nibh non nulla, pretium aenean in ipsum neque placerat congue, eleifend ornare quia ut in eos libero. Volutpat eros, commodo erat quam elit, sit laoreet nibh eum velit. Nibh pellentesque, purus amet congue. Pede felis sed felis, platea proin sodales sodales. Aliquam laoreet beatae congue, sed nulla bibendum sit lorem vitae, nec interdum posuere amet viverra eros. Consequat phasellus magna purus molestie justo orci, donec nibh lacus felis egestas quam, eget ut vitae, in suspendisse, duis eget vestibulum porta eget suspendisse sed.</p>
 			</div>
 		</div>
   </div>
@@ -92,6 +125,7 @@
 <footer class="footer">
 	<div class="footerContainer">
 		<p>2015 | Tutoring Portal</p>
+	</div>
 </footer>
 </div><!--Container-->
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
